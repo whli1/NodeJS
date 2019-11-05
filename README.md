d6d06cb6478c3b0e2322208d628368d139bb27aa
whli <whl802302@outlook.com>
2019-11-05 10:50:55 +0800

    根据不同访问路径返回不同数据

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# NodeJS
+# Node.JS 
+
+>node.js是什么
+```
+nodejs是JavaScript运行时的平台；既不是语言也不是框架
+```
+
+>node.js中的JavaScript
+```
+1、没有BOM和DOM
+2、ECMAScript基本的JavaScript部分
+3、在Node中为JavaScript提供了一些服务器级别的API
+    例如：
+        *文件操作能力
+        *http的服务能力
+```
--- day01/code/http.js
+++ day01/code/http.js
@@ -10,16 +10,57 @@ const server = http.createServer()
 
 //3、用来提供服务：发送请求 接受请求 处理请求 反馈(发送响应)
 
-    //注册request请求事件，当接收到客户端的请求时，自动触发 request 请求事件，然后知新规调函数
+//注册request请求事件，当接收到客户端的请求时，自动触发 request 请求事件，然后知新规调函数
 server.on('request', (request, response) => {
     console.log(`收到客户端的请求,请求路径是${request.url}`);
-    response.write('hello');
-    //必须要有end
-    response.end();
+    /**
+     *  response.write('hello');
+     *  response.end();
+     *  这种方式比较繁琐，推荐在end时直接返回响应数据
+     *  response.end('hello world');
+     */
+
+    /**
+     * 根据不同访问路径返回不同数据
+     *
+     */
+    switch (request.url) {
+        case '/':
+            response.end('hello');
+            break;
+        case '/a':
+            response.end('hello a');
+            break;
+        case '/login':
+            response.end('hello login');
+            break;
+        case '/product':
+            let product = [
+                {
+                    name: '苹果',
+                    price: '4.5'
+                },
+                {
+                    name: '香蕉',
+                    price: '2.5'
+                },
+                {
+                    name: '西瓜',
+                    price: '0.5'
+                }
+            ];
+        //    响应内容只能是二进制数据或者字符串，数组对象布尔值都不可用
+            response.end(JSON.stringify(product));
+        default:
+            response.end('404 Not Found');
+            break;
+    }
+
+
 })
 
 //4、绑定端口号，启动服务器
-server.listen(3000,()=>{
+server.listen(3000, () => {
     console.log('服务器启动成功，可以通过http://127.0.0.1:3000/进行访问');
 });
 
